a1a5cf89b126119a2585c120671bbc37ac813792
Nihal <nagasainihal_allam@srmap.edu.in>
2026-02-11 23:10:08 +0530

    Mean Median Mode imputations

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -22,3 +22,15 @@ def mean_impute_train_test(X_train, X_test):
     X_train_imputed = imputer.fit_transform(X_train)
     X_test_imputed = imputer.transform(X_test)
     return X_train_imputed, X_test_imputed
+
+def median_impute_train_test(X_train, X_test):
+    imputer = SimpleImputer(strategy="median")
+    X_train_imputed = imputer.fit_transform(X_train)
+    X_test_imputed = imputer.transform(X_test)
+    return X_train_imputed, X_test_imputed
+
+def mode_impute_train_test(X_train, X_test):
+    imputer = SimpleImputer(strategy="most_frequent")
+    X_train_imputed = imputer.fit_transform(X_train)
+    X_test_imputed = imputer.transform(X_test)
+    return X_train_imputed, X_test_imputed
